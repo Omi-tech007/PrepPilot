0c9369d478475a15adcbbf0c03def6b7a91c0895
Omkar Gunjal <omkarbg007@gmail.com>
2026-01-11 11:33:34 +0530

    update domain

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: Replace the values below with your NEW Firebase project config
 const firebaseConfig = {
   apiKey: "AIzaSyC310A6fhBb04sOmUjV_vFoZsOxDpUhc7E",
   authDomain: "preppilotapp.firebaseapp.com",
@@ -18,4 +15,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// EXPORT these so App.jsx can use them
+export const auth = getAuth(app); 
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+export default app;
